c3425d17af8003c1db3bbf92629ea358c48f199d
Pawel Gajzler <pawel.gajzler.pg@gmail.com>
2024-12-03 10:25:32 +0100

    day 3 completed

---
+++ AdventOfCode2024/AdventOfCode.Day3/MulRegexGenerator.cs
@@ -0,0 +1,52 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode2024.Day3
+{
+    internal static partial class MulRegexGenerator
+    {
+        [GeneratedRegex("mul\\((?<first>\\d+),(?<second>\\d+)\\)", RegexOptions.IgnoreCase, "en-US")]
+        private static partial Regex MulGeneratedRegex();
+
+        public static long GetResultFromLine(string line)
+        {
+            long result = 0;
+            foreach (Match match in MulGeneratedRegex().Matches(line))
+            {
+                long mulresult = long.Parse(match.Groups["first"].Value)
+                    * long.Parse(match.Groups["second"].Value);
+                result += mulresult;
+            }
+
+            return result;
+        }
+
+        public static long GetResultFromLineWithDoDontInstructions(string line)
+        {
+            long result = 0;
+            foreach (Match match in MulGeneratedRegex().Matches(line))
+            {
+                bool allowToResult = GetLastInstructionForCalculation(line, match.Index);
+
+                if (!allowToResult)
+                    continue;
+
+                long mulresult = long.Parse(match.Groups["first"].Value)
+                    * long.Parse(match.Groups["second"].Value);
+
+
+                result += mulresult;
+            }
+
+            return result;
+        }
+
+        private static bool GetLastInstructionForCalculation(string line, int index)
+        {
+            string lineFragment = line.Substring(0, index);
+            int doIndex = lineFragment.LastIndexOf("do()");
+            int doNotIndex = lineFragment.LastIndexOf("don't()");
+
+            return doNotIndex == -1 || doIndex > doNotIndex;
+        }
+    }
+}
---
+++ AdventOfCode2024/AdventOfCode.Day3/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AdventOfCode2024.Day3;
+
+Console.WriteLine("Hello, World!");
+
+string lines = await File.ReadAllTextAsync("input.txt");
+
+long finalResult = MulRegexGenerator.GetResultFromLine(lines);
+long adjustedFinalResult = MulRegexGenerator.GetResultFromLineWithDoDontInstructions(lines);
+
+Console.WriteLine($"Final Result: {finalResult}");
+Console.WriteLine($"Adjusted Final Result: {adjustedFinalResult}");
