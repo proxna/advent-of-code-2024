b75c7fe60aa94d8fef868b6e14e3594265eba229
Pawel Gajzler <pawel.gajzler.pg@gmail.com>
2024-12-21 00:16:29 +0100

    day 19 and day 20

---
+++ AdventOfCode2024/AdventOfCode2024.Day19/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+string input = await File.ReadAllTextAsync("input.txt");
+
+string[] inputParts = input.Split("\r\n\r\n");
+
+string[] patterns = inputParts[0].Split(',', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).OrderByDescending(s => s.Length).ToArray();
+
+string[] towels = inputParts[1].Split("\r\n", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+
+Dictionary<string, long> memo = new() { [string.Empty] = 1L };
+
+Console.WriteLine(towels.Count(design => Permute(design, patterns, memo) > 0));
+Console.WriteLine(towels.Sum(design => Permute(design, patterns, memo)));
+
+static long Permute(string design, string[] patterns, Dictionary<string, long> memo)
+{
+    if (memo.TryGetValue(design, out var value))
+    {
+        return value;
+    }
+
+    memo[design] = patterns
+        .Where(design.StartsWith)
+        .Sum(pattern => Permute(design[pattern.Length..], patterns, memo));
+    return memo[design];
+}
---
+++ AdventOfCode2024/AdventOfCode2024.Day20/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Numerics;
+
+int Solve(string input, int cheat)
+{
+    var path = GetPath(input);
+    var indices = Enumerable.Range(0, path.Length).ToArray();
+
+    // sum up the worthy cheats for each index along the path
+    var cheatsFromI = (int i) => (
+        from j in indices[0..i]
+        let dist = Manhattan(path[i], path[j])
+        let saving = i - (j + dist)
+        where dist <= cheat && saving >= 100
+        select 1
+    ).Sum();
+
+    // parallel is gold today, it gives us an 3-4x boost
+    return indices.AsParallel().Select(cheatsFromI).Sum();
+}
+
+int Manhattan(Complex a, Complex b) =>
+    (int)(Math.Abs(a.Imaginary - b.Imaginary) + Math.Abs(a.Real - b.Real));
+
+// Follow the path from finish to start, supposed that there is a single track in the input.
+// The index of a position in the returned array equals to its distance from the finish
+Complex[] GetPath(string input)
+{
+    var lines = input.Split("\r\n");
+    var map = (
+        from y in Enumerable.Range(0, lines.Length)
+        from x in Enumerable.Range(0, lines[0].Length)
+        select new KeyValuePair<Complex, char>(x + y * Complex.ImaginaryOne, lines[y][x])
+    ).ToDictionary();
+
+    Complex[] dirs = [-1, 1, Complex.ImaginaryOne, -Complex.ImaginaryOne];
+
+    var start = map.Keys.Single(k => map[k] == 'S');
+    var goal = map.Keys.Single(k => map[k] == 'E');
+
+    var (prev, cur) = ((Complex?)null, goal);
+    var res = new List<Complex> { cur };
+
+    while (cur != start)
+    {
+        var dir = dirs.Single(dir => map[cur + dir] != '#' && cur + dir != prev);
+        (prev, cur) = (cur, cur + dir);
+        res.Add(cur);
+    }
+    return res.ToArray();
+}
+
+string input = await File.ReadAllTextAsync("input.txt");
+
+Console.WriteLine(Solve(input, 2));
+Console.WriteLine(Solve(input, 20));
