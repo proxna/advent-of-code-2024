e920608d1314f6f2343760504e53b284a7e44e8c
Pawel Gajzler <pawel.gajzler.pg@gmail.com>
2024-12-23 13:58:12 +0100

    day 22 23

---
+++ AdventOfCode2024/AdventOfCode2024.Day22/Program.cs
@@ -0,0 +1,139 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net;
+
+Console.WriteLine("Hello, World!");
+
+string[] lines = await File.ReadAllLinesAsync("input.txt");
+long[] firstNumbers = lines.Select(s => long.Parse(s)).ToArray();
+
+int i = 0;
+
+while (i < 2000)
+{
+    for (int j = 0; j < firstNumbers.Length; j++)
+    {
+        firstNumbers[j] = StepOne(firstNumbers[j]);
+        firstNumbers[j] = StepTwo(firstNumbers[j]);
+        firstNumbers[j] = StepThree(firstNumbers[j]);
+    }
+
+    i++;
+}
+
+long result = firstNumbers.Sum();
+
+Console.WriteLine(result);
+
+List<List<int>> sequences = new List<List<int>>();
+
+for(int j = 0; j < firstNumbers.Length; j++)
+{
+    sequences.Add(new List<int> { -1 });
+}
+
+firstNumbers = lines.Select(s => long.Parse(s)).ToArray();
+
+i = 0;
+
+while (i < 2000)
+{
+    for (int j = 0; j < firstNumbers.Length; j++)
+    {
+        firstNumbers[j] = StepOne(firstNumbers[j]);
+        firstNumbers[j] = StepTwo(firstNumbers[j]);
+        firstNumbers[j] = StepThree(firstNumbers[j]);
+        sequences[j].Add((int)(firstNumbers[j] % 10));
+    }
+
+    i++;
+}
+
+Console.WriteLine();
+
+var input = File.ReadAllLines("input.txt");
+MonkeyMarket(input, out var part1, out var part2, iterations: 2000);
+
+Console.WriteLine($"Part 1: {part1}");
+Console.WriteLine($"Part 2: {part2}");
+
+// Part 1 can be super small, actually:
+var part1OneLiner = input.Select(int.Parse).Sum(seed => (long)Mrng(seed).Skip(2_000).First());
+Console.WriteLine($"Part 1 (short): {part1OneLiner}");
+
+static IEnumerable<int> Mrng(int seed)
+{
+    var next = seed;
+    while (true)
+    {
+        yield return next;
+        next = (next ^ (next << 6)) & 0x00FFFFFF;
+        next = (next ^ (next >> 5)) & 0x00FFFFFF;
+        next = (next ^ (next << 11)) & 0x00FFFFFF;
+    }
+}
+
+void MonkeyMarket(IEnumerable<string> seeds, out long part1, out int part2, int iterations = 2000)
+{
+    var intSeeds = seeds.Select(int.Parse).ToArray();
+    var tt = new Dictionary<MarketChanges, int>();
+    part1 = 0;
+
+    foreach (var seed in intSeeds)
+    {
+        using var rng = Mrng(seed).GetEnumerator();
+        var cc = new HashSet<MarketChanges>();
+        var carry = new sbyte[4];
+        int? last = default;
+        for (int i = 0; i <= iterations && rng.MoveNext(); ++i)
+        {
+            int curr = rng.Current;
+            var value = curr % 10;
+            if (last.HasValue)
+            {
+                carry[i % 4] = (sbyte)(value - last.Value);
+            }
+
+            if (i >= 4)
+            {
+                var key = new MarketChanges(carry[i % 4], carry[(i - 1) % 4], carry[(i - 2) % 4], carry[(i - 3) % 4]);
+                if (cc.Add(key))
+                {
+                    tt[key] = tt.GetValueOrDefault(key) + value;
+                }
+            }
+
+            last = value;
+        }
+        part1 += rng.Current;
+    }
+
+    part2 = tt.Values.Max();
+}
+
+long Mix(long firstNumber, long secondNumber)
+{
+    return firstNumber^secondNumber;
+}
+
+long Prune(long number)
+{
+    return number % 16777216;
+}
+
+long StepOne(long number)
+{
+    return Prune(Mix(number * 64, number));
+}
+
+long StepTwo(long number)
+{
+    long newNumber = (long)Math.Round((double)(number / 32), 0);
+    return Prune(Mix(newNumber, number));
+}
+
+long StepThree(long number)
+{
+    return Prune(Mix(number * 2048, number));
+}
+
+internal readonly record struct MarketChanges(sbyte A, sbyte B, sbyte C, sbyte D);
---
+++ AdventOfCode2024/AdventOfCode2024.Day23/Program.cs
@@ -0,0 +1,94 @@
+﻿using Graph = System.Collections.Generic.Dictionary<string, System.Collections.Generic.HashSet<string>>;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string[] input = await File.ReadAllLinesAsync("input.txt");
+
+List<(string, string)> lines = new();
+
+foreach (string item in input)
+{
+    string[] lineHosts = item.Split('-');
+    var conn = (lineHosts[0], lineHosts[1]);
+    lines.Add(conn);
+    lines.Add(Swap(conn));
+}
+
+var tHosts = lines.Where(h => h.Item1.StartsWith('t')).Select(h => h.Item1).Distinct();
+
+HashSet<(string, string, string)> connections = new();
+
+foreach (string tHost in tHosts)
+{
+    var connectedHostsToT = lines.Where(h => h.Item1 == tHost).Select(h => h.Item2).Distinct();
+    foreach (string connectedHost in connectedHostsToT)
+    {
+        var connectedHosts = lines.Where(h => h.Item1 == connectedHost).Select(h => h.Item2).Distinct();
+        foreach (string thirdHostCandidate in connectedHosts)
+        {
+            if (connectedHostsToT.Contains(thirdHostCandidate))
+            {
+                connections.Add((tHost, connectedHost, thirdHostCandidate));
+                connections.Add((tHost, thirdHostCandidate, connectedHost));
+                connections.Add((thirdHostCandidate, connectedHost, tHost));
+                connections.Add((thirdHostCandidate, tHost, connectedHost));
+                connections.Add((connectedHost, tHost, thirdHostCandidate));
+                connections.Add((connectedHost, thirdHostCandidate, tHost));
+            }
+        }
+    }
+}
+
+foreach (var connectedHost in connections)
+{
+    Console.WriteLine($"{connectedHost.Item1},{connectedHost.Item2},{connectedHost.Item3}");
+}
+
+Console.WriteLine($"{connections.Count / 6}");
+
+Console.WriteLine(PartTwo(input));
+
+Graph GetGraph(string[] lines)
+{
+    var edges =
+        from line in lines
+        let nodes = line.Split("-")
+        from edge in new[] { (nodes[0], nodes[1]), (nodes[1], nodes[0]) }
+        select (From: edge.Item1, To: edge.Item2);
+
+    return (
+         from e in edges
+         group e by e.From into g
+         select (g.Key, g.Select(e => e.To).ToHashSet())
+     ).ToDictionary();
+}
+
+string PartTwo(string[] lines)
+{
+    var g = GetGraph(lines);
+    var components = GetSeed(g);
+    while (components.Count > 1)
+    {
+        components = Grow(g, components);
+    }
+    return components.Single();
+}
+
+HashSet<string> GetSeed(Graph g) => g.Keys.ToHashSet();
+
+HashSet<string> Grow(Graph g, HashSet<string> components) => (
+    from c in components.AsParallel()
+    let members = Members(c)
+    from neighbour in members.SelectMany(m => g[m]).Distinct()
+    where !members.Contains(neighbour)
+    where members.All(m => g[neighbour].Contains(m))
+    select Extend(c, neighbour)
+).ToHashSet();
+
+IEnumerable<string> Members(string c) =>
+    c.Split(",");
+string Extend(string c, string item) =>
+    string.Join(",", Members(c).Append(item).OrderBy(x => x));
+
+(T, T) Swap<T>((T, T) input) => (input.Item2, input.Item1);
